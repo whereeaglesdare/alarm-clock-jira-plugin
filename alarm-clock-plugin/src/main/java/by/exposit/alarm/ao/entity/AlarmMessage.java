bc94ce73981eda45b09b67c85e26caf4d7422974
bbelokur <belokur.bv@gmail.com>
2019-11-24 19:12:02 +0200

    Add fields to AO relation ALARM_MESSAGE

--- alarm-clock-plugin/src/main/java/by/exposit/alarm/ao/entity/AlarmMessage.java
+++ alarm-clock-plugin/src/main/java/by/exposit/alarm/ao/entity/AlarmMessage.java
@@ -1,11 +1,12 @@
 package by.exposit.alarm.ao.entity;
 
+import net.java.ao.Entity;
 import net.java.ao.schema.Table;
 
 import java.util.Date;
 
 @Table("ALARM_MESSAGE")
-public interface AlarmMessage {
+public interface AlarmMessage extends Entity {
     Long getUserId();
 
     void setUserId(Long userId);
@@ -17,4 +18,16 @@ public interface AlarmMessage {
     Date getAlarmDate();
 
     void setAlarmDate(Date alarmDate);
+
+    Boolean isAcknowledged();
+
+    void setAcknowledged(Boolean isAcknowledged);
+
+    Boolean isRemoved();
+
+    void setRemoved(Boolean isRemoved);
+
+    Boolean isAdministrative();
+
+    void setAdministrative(Boolean isAdministrative);
 }
