a021d436c286c4ab261a9b7ec91842c23febb4d6
bbelokur <belokur.bv@gmail.com>
2019-11-26 22:26:22 +0200

    Adding javadoc and removing method for creating bulk alarms

--- alarm-clock-plugin/src/main/java/by/exposit/alarm/ao/dao/AlarmMessageDao.java
+++ alarm-clock-plugin/src/main/java/by/exposit/alarm/ao/dao/AlarmMessageDao.java
@@ -6,16 +6,49 @@ import com.atlassian.activeobjects.tx.Transactional;
 import java.util.Date;
 import java.util.List;
 
+/**
+ *  Interface for working with {@link by.exposit.alarm.ao.entity.AlarmMessage} entity
+ *  Administrative and user alarms stored in one table.
+ *
+ * @author Belokur Bohdan
+ */
 public interface AlarmMessageDao {
+    /**
+     * Creating AlarmMessage entity
+     *
+     * @param userId ApplicationUser id
+     * @param message description of alarm
+     * @param alertDate date, when alarm should trigger
+     * @param isAdministrative true for alarms created by Jira Administrator or System administrator
+     * @return {@link by.exposit.alarm.ao.entity.AlarmMessage} entity
+     */
     @Transactional
     AlarmMessage createAlertMessage(Long userId, String message, Date alertDate, Boolean isAdministrative);
 
+    /**
+     * Updating AlarmMessage entity
+     *
+     * @param alarmMessageId id of alarm
+     * @param message alarm description
+     * @param alertDate alarm date
+     * @return updated AlarmMessage entity
+     */
     @Transactional
     AlarmMessage updateAlarmMessage(int alarmMessageId, String message, Date alertDate);
 
+    /**
+     * "Removing" alarm message.
+     * Alarm does not removed from database. "Removed" property setting to true.
+     * @param alarmMessage AlarmMessage entity
+     */
     @Transactional
     void removeAlarmMessage(AlarmMessage alarmMessage);
 
+    /**
+     * Acknowledge alarm
+     * <p>User should acknowledge alarm to hide it</p>
+     * @param alarmMessage AlarmMessage entity
+     */
     @Transactional
     void acknowledgeAlarm(AlarmMessage alarmMessage);
 
@@ -23,12 +56,14 @@ public interface AlarmMessageDao {
 
     List<AlarmMessage> getAlarmMessagesbyUserId(Long userId);
 
-    List<AlarmMessage> bulkCreateAlarmMessages(List<Long> userIds, String message,
-                                               Date alertDate);
-
     List<AlarmMessage> getAdministrativeAlarmMessages();
 
     List<AlarmMessage> getAllAlarmMessages();
 
+    /**
+     * Getting all unacknowledge AlarmMessages
+     *
+     * @return List of AlarmMessages
+     */
     List<AlarmMessage> getActiveAlarmMessages();
 }
