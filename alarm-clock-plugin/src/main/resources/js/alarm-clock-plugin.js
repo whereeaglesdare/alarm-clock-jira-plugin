d9b7505ace88a2fb7254fc9c1398911145e6a61a
bbelokur <belokur.bv@gmail.com>
2019-11-28 21:34:45 +0200

    Adding AJAX table loading, creating alarms in user profile

--- alarm-clock-plugin/src/main/resources/js/alarm-clock-plugin.js
+++ alarm-clock-plugin/src/main/resources/js/alarm-clock-plugin.js
@@ -1,3 +1,60 @@
 AJS.$(document).ready(function() {
-    AJS.$('#demo-range-2').datePicker({'overrideBrowserDefault': true});
+    AJS.$('#alarm-date').datePicker({'overrideBrowserDefault': true});
+
+    function setTableValues(selector, integration, status, dateFrom, dateTo, action) {
+        $.ajax({
+            dataType: "json",
+            url: AJS.contextPath() + "/rest/alarm/1.0/alarm",
+        }).done(function(msg) {
+             var tbody =  document.getElementById("alarm-table-body");
+             for (i = 0; i < msg.length; i++) {
+                addRow(tbody, msg[i].date, msg[i].description, msg[i].isAcknowledged, msg[i].isAdministrative);
+             }
+        });
+    }
+
+    function addRow(tbody, date, description, isAcknowledged, isAdministrative) {
+        var row = tbody.insertRow(-1);
+        var d = new Date();
+        row.insertCell(0).innerHTML = getFormattedDate(new Date(date));
+        row.insertCell(1).innerHTML  = description;
+    }
+
+    function getFormattedDate(m) {
+        return m.getUTCFullYear() +"/"+ (m.getUTCMonth()+1) +"/"+ m.getUTCDate() + " " + m.getUTCHours() + ":" + m.getUTCMinutes();
+    }
+
+    AJS.$("#alarm-create-button").on('click', function (e) {
+        var formData = {
+            "date": new Date($("#alarm-date").val() + " " + $("#time-input").val()).getTime(),
+            "description": $("#description-input").val()
+        }
+        $.ajax({
+            type: 'POST',
+            data: JSON.stringify(formData),
+            dataType: 'json',
+            encode: true,
+            contentType: 'application/json',
+            url: AJS.contextPath() + "/rest/alarm/1.0/alarm"
+        })
+        .done(function(data) {
+             var myFlag = AJS.flag({
+                 type: 'success',
+                 body: "Your alarm successfully created",
+                 close: "auto"
+             });
+        })
+        .fail(function(data) {
+             if (data.message) {
+                  AJS.flag({
+                     type: 'error',
+                     body: data.message,
+                     close: "auto"
+                 });
+             }
+        });
+     });
+
+    setTableValues("alarms-table");
 });
+
